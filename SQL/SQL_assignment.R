91f1e8918728f836cfdc57124498e8677db1944c
Yuanyuan Lin <yuanyuanlin@dhcp-wifi-8021x-168-122-215-3.bu.edu>
2019-10-31 20:04:15 -0400

    commit

--- SQL/SQL_assignment.R
+++ SQL/SQL_assignment.R
@@ -12,10 +12,33 @@ library(dplyr)
 
 
 #remove NA misclassifications and white space in each field
+
+
 contrib_all <- read_xlsx("Top MA Donors 2016-2020.xlsx", sheet="Direct Contributions JFC Dist",na = c("", "N/A", "NA"), trim_ws = TRUE)
 JFC<-read_xlsx("Top MA Donors 2016-2020.xlsx",sheet="JFC Contributions",na = c("", "N/A", "NA"), trim_ws = TRUE)
 
 
+contrib_all['contrib'] = gsub(contrib_all$contrib,pattern = ", ",replacement = ",")
+contrib_all['contrib'] = gsub(contrib_all$contrib,pattern = "\\s\\w*",replacement = "")
+
+(co<-unique(contrib_all$contrib))
+contrib_all['contrib'] = gsub(contrib_all$contrib,pattern = ",,",replacement = "")
+
+(co<-unique(contrib_all$contrib))
+
+contrib_all['contrib'] = gsub(contrib_all$contrib,pattern = "HN,*",replacement = "HN")
+(co<-unique(contrib_all$contrib))
+
+contrib_all['contrib'] = gsub(contrib_all$contrib,pattern = "E\\sH",replacement = "E")
+contrib_all['contrib'] = gsub(contrib_all$contrib,pattern = "H\\sH",replacement = "H")
+contrib_all['contrib'] = gsub(contrib_all$contrib,pattern = "OS,",replacement = "OS")
+contrib_all['contrib'] = gsub(contrib_all$contrib,pattern = "RD\\sB",replacement = "RD")
+contrib_all['contrib'] = gsub(contrib_all$contrib,pattern = "AN\\sB",replacement = "AN")
+contrib_all['contrib'] = gsub(contrib_all$contrib,pattern = "LD\\sMR",replacement = "LD")
+contrib_all['contrib'] = gsub(contrib_all$contrib,pattern = "TA\\sS",replacement = "TA")
+contrib_all['contrib'] = gsub(contrib_all$contrib,pattern = "UL\\sL",replacement = "UL")
+contrib_all['contrib'] = gsub(contrib_all$contrib,pattern = "AN\\sS",replacement = "AN")
+contrib_all['contrib'] = gsub(contrib_all$contrib,pattern = "CE\\sH",replacement = "CE")
 
 #merge two dataframes horizontally
 contrib_all<-rbind(contrib_all,JFC)
@@ -34,7 +57,7 @@ contrib_all <- contrib_all %>%
 #unique column or set of columns (the primary key).
 
 contributors<-select(contrib_all,contribid,contrib,City,State,Zip,fam,Fecoccemp,date,amount,type)
-orgs<-select(contrib_all,orgname,ultorg)
+orgs<-select(contrib_all,orgname)
 recipients<-select(contrib_all,recipid,recipient,party,recipcode,cmteid)
 cycle<-select(contrib_all,cycle,fectransid)
 
@@ -44,7 +67,7 @@ cycle<-select(contrib_all,cycle,fectransid)
 #Create relationships between these new tables and their predecessors through the use of foreign keys
 contributors<-select(contrib_all,contribid,contrib,City,State,Zip,date,amount)
 contributors_tbl<-select(contrib_all,contrib,Fecoccemp,fam,type)
-orgs<-select(contrib_all,orgname,ultorg)
+orgs<-select(contrib_all,orgname)
 recipients<-select(contrib_all,recipid,recipient,party,recipcode,cmteid)
 cycle<-select(contrib_all,cycle,fectransid)
 
@@ -57,6 +80,7 @@ orgs<-select(contrib_all,contribid,orgname)
 recipients<-select(contrib_all,recipid,recipient,contribid)
 recipients_type<-select(contrib_all,recipid,party,recipcode,cmteid,contribid)
 cycle<-select(contrib_all,contribid,cycle,fectransid)
+donors<-select(contrib_all,contrib,contribid)
 #A relation is in 4NF if it has no multi-valued dependencies
 #Removing duplicate data entries
 contributors <- contributors %>%
@@ -71,10 +95,15 @@ recipients_type <- recipients_type %>%
   distinct()
 cycle <- cycle %>%
   distinct()
+donors <- donors %>%
+  distinct()
 
-write.csv(contributors,file="Donors.csv")
-write.csv(contributors_tbl,file="contributors_tbl.csv")
-write.csv(orgs,file="orgs.csv")
-write.csv(recipients,file="recipients.csv")
-write.csv(recipients_type,file="recipients_type.csv")
-write.csv(cycle,file="cycle.csv")
+polcont<-dbConnect(SQLite(),"pol_cont")
+
+dbWriteTable(polcont, "contributors", contributors)
+dbWriteTable(polcont, "contributors_tbl", contributors_tbl)
+dbWriteTable(polcont, "orgs", orgs)
+dbWriteTable(polcont, "recipients", recipients)
+dbWriteTable(polcont, "recipients_type", recipients_type)
+dbWriteTable(polcont, "cycle", cycle)
+dbWriteTable(polcont, "donors", donors)
